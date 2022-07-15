0f1c89a2fa02497a533bcf6b66265d62ee75f896
Antonio Salazar <antsalgra@hotmail.com>
2022-07-15 08:59:40 -0700

    Archivo actualizado

--- CreateObjects.ps1
+++ CreateObjects.ps1
@@ -1,6 +1,19 @@
+param([Parameter(Mandatory=$true)] $jsonFile)
 
+function CreateADGroup(){
+    param([Parameter(Mandatory=$true)] $groupObject)
 
+    # Tomo el nombre del grupo.
+    $name = $groupObject.name
 
+    # Creo el grupo en ADDS, si no existe
+    try {
+        New-ADGroup -name $name -GroupScope Global
+        Write-Output "Se ha creado el grupo $name"
+    } catch {
+        Write-Warning "Error al crear el grupo $name. Puede que ya exista"
+    }
+}
 
 # Objeto JSON que contiene el esquema de ADDS.
 $json = (Get-content $jsonFile | ConvertFrom-JSON)
